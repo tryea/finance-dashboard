e3c1d7a9adafbfa850879a7388675989391865ff
aristoersapta@gmail.com <aristoersapta@gmail.com>
2023-04-18 07:32:55 +0700

    [main] adding chart for revenue and profit on box b

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,3 +1,4 @@
+import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
 import { useTheme } from "@mui/material";
@@ -5,11 +6,14 @@ import { useMemo } from "react";
 import {
     ResponsiveContainer,
     AreaChart,
-    CartesianGrid,
     XAxis,
     YAxis,
     Tooltip,
     Area,
+    Line,
+    LineChart,
+    CartesianGrid,
+    Legend,
 } from "recharts";
 
 const Row1 = () => {
@@ -28,22 +32,78 @@ const Row1 = () => {
             })
         )
     }, [data])
+
+    const revenueProfit = useMemo(() => {
+        return (
+            data &&
+            data[0].monthlyData.map(({ month, revenue, expenses }) => {
+                return {
+                    name: month.substring(0, 3),
+                    revenue: revenue,
+                    profit: (revenue - expenses).toFixed(2),
+                };
+            })
+        );
+    }, [data]);
+
     return (
         <>
             <DashboardBox gridArea="a">
+                <BoxHeader
+                    title="Revenue and Expenses"
+                    subtitle="top line represents revenue, bottom line represents expenses"
+                    sideText="+4%"
+                />
                 <ResponsiveContainer width="100%" height="100%" >
                     <AreaChart
                         width={500}
                         height={400}
                         data={revenueExpenses}
                         margin={{
-                            top: 10,
-                            right: 30,
-                            left: 0,
-                            bottom: 0
+                            top: 15,
+                            right: 25,
+                            left: -10,
+                            bottom: 60
                         }}
                     >
-                        <CartesianGrid strokeDasharray="0 3" />
+                        <defs>
+                            <linearGradient
+                                id="colorRevenue"
+                                x1="0"
+                                y1="0"
+                                x2="0"
+                                y2="1"
+                            >
+                                <stop
+                                    offset="5%"
+                                    stopColor={palette.primary[300]}
+                                    stopOpacity={0.5}
+                                />
+                                <stop
+                                    offset="95%"
+                                    stopColor={palette.primary[300]}
+                                    stopOpacity={0}
+                                />
+                            </linearGradient>
+                            <linearGradient
+                                id="colorExpenses"
+                                x1="0"
+                                y1="0"
+                                x2="0"
+                                y2="1"
+                            >
+                                <stop
+                                    offset="5%"
+                                    stopColor={palette.primary[300]}
+                                    stopOpacity={0.5}
+                                />
+                                <stop
+                                    offset="95%"
+                                    stopColor={palette.primary[300]}
+                                    stopOpacity={0}
+                                />
+                            </linearGradient>
+                        </defs>
                         <XAxis
                             dataKey="name"
                             tickLine={false}
@@ -76,12 +136,74 @@ const Row1 = () => {
                             dot={true}
                             stroke={palette.primary.main}
                             fillOpacity={1}
-                            fill="url(#colorRevenue)"
+                            fill="url(#colorExpenses)"
                         />
                     </AreaChart>
                 </ResponsiveContainer>
             </DashboardBox>
             <DashboardBox gridArea="b">
+                <BoxHeader
+                    title="Profit and Revenue"
+                    sideText="+4%"
+                />
+                <ResponsiveContainer width="100%" height="100%" >
+                    <LineChart
+                        width={500}
+                        height={400}
+                        data={revenueProfit}
+                        margin={{
+                            top: 15,
+                            right: 25,
+                            left: -10,
+                            bottom: 60
+                        }}
+                    >
+                        <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+                        <XAxis
+                            dataKey="name"
+                            tickLine={false}
+                            style={{
+                                fontSize: "10px"
+                            }}
+                        />
+                        <YAxis
+                            yAxisId='left'
+                            tickLine={false}
+                            axisLine={false}
+                            style={{
+                                fontSize: "10px",
+                            }}
+                        />
+                        <YAxis
+                            yAxisId='right'
+                            tickLine={false}
+                            axisLine={false}
+                            style={{
+                                fontSize: "10px",
+                            }}
+                            orientation="right"
+                        />
+                        <Tooltip />
+                        <Legend
+                            height={20}
+                            wrapperStyle={{
+                                margin: "0 0 10px 0",
+                            }}
+                        />
+                        <Line
+                            yAxisId={'left'}
+                            type={"monotone"}
+                            dataKey={"profit"}
+                            stroke={palette.tertiary[500]}
+                        />
+                        <Line
+                            yAxisId={'right'}
+                            type={"monotone"}
+                            dataKey={"revenue"}
+                            stroke={palette.primary.main}
+                        />
+                    </LineChart>
+                </ResponsiveContainer>
             </DashboardBox>
             <DashboardBox gridArea="c">
             </DashboardBox>
